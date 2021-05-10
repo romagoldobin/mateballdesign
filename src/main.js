8cd9a1b91506643d2199bf75e457b056cc8480d2
romagoldobin <goldobinforwork@gmail.com>
2021-05-10 14:42:32 +0500

    Added body settings and main colors

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+import './assets/font.css';
+import './assets/styles.scss';
+
 Vue.config.productionTip = false;
 
 new Vue({
