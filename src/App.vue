07c5381a2ad0701fb934733b0bffba0e443a5e3f
romagoldobin <goldobinforwork@gmail.com>
2021-05-10 21:13:46 +0500

    added base layout and styles for it

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <section class="main">
+      <header class="header"></header>
+      <div class="main__content">
+        <router-view />
+      </div>
+    </section>
   </div>
 </template>
 
@@ -8,6 +14,18 @@
   flex: 1;
   overflow: hidden;
   display: flex;
+  flex-direction: column;
 }
 
+.main {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  height: 100%;
+  padding: 28px 24px;
+  overflow: hidden;
+}
+
+.header {
+  padding: 20px 10px;
+}
 </style>
